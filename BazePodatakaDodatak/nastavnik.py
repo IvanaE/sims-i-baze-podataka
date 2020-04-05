291eaee4da34dcf7dae226860f30ef087abf39bb
NadaRajlic18 <nada.rajlic.18@singimail.rs>
2020-04-05 22:33:12 +0200

    nova Astah tabela sa UseCaseDiagramom i ClassDiagramom

--- BazePodatakaDodatak/nastavnik.py
+++
@@ -1,7 +0,0 @@
-class Nastavnik():
-    def __init__(self, sifra_zaposlenog, ime, prezime, datum_rodjenja, tip_predavaca):
-        self.sifra_zaposlenog = sifra_zaposlenog
-        self.ime = ime
-        self.prezime = prezime
-        self.datum_rodjenja = datum_rodjenja
-        self.tip_predavaca = tip_predavaca
