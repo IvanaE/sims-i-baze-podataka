247b875b5a181318e88c08e732386989d56512d5
Nada Rajlic <nada.rajlic.18@singimail.rs>
2020-06-20 19:42:19 +0200

    Upravljanje korisnicima

---
+++ RukovalacInfomacionimResursima/mysql/data_handler.py
@@ -0,0 +1,22 @@
+class DataHandler:
+
+    def __init__(self):
+        super().__init__()
+
+    def get_one(self, id):
+        pass
+    
+    def get_all(self):
+        pass
+
+    def edit(self, obj):
+        pass
+
+    def delete_one(self, obj):
+        pass
+
+    def insert(self, obj):
+        pass
+
+    def insert_many(self, list):
+        pass
